ff4bf323475f370939659b9a8aded24cab146255
Beatriz <beatriz.mediatto2@gmail.com>
2022-08-29 18:02:51 -0300

    arruma requisito 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,9 +1,8 @@
-const { species } = require('../data/zoo_data');
+// const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const [leao, tigre, ursos, penguins, lontras, sapos, cobras, elefantes, girafas] = species;
-  
+
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,12 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
- if(typeof employeeName === undefined) {
-  return {};
- }
- return data.employees.find((elemento) => {
-  return elemento.firstName === employeeName || elemento.lastName === employeeName; 
- })
+  if (employeeName === undefined) {
+    return {};
+  }
+  return data.employees.find((elemento) => {
+    return elemento.firstName === employeeName || elemento.lastName === employeeName;
+  });
 }
 
 module.exports = getEmployeeByName;
