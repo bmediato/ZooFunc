080f65da162922d3c8829b5e3762d32b04c27b4e
Beatriz <beatriz.mediatto2@gmail.com>
2022-08-29 19:10:15 -0300

    requisito 1 feito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,19 +2,10 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-
   if (ids.length === 0) {
     return [];
   }
-  return species.filter((elemento) => {
-    if (ids.length === 1) {
-      const id = ids.toString();
-      return elemento.id === id;
-    }
-    return ids.forEach((element) => {
-      return elemento.id === element;
-    })
-  })
+  return species.filter((elemento) => ids.includes(elemento.id))
 }
 
 module.exports = getSpeciesByIds;
