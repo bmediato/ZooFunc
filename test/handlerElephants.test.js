cad1e0cc763d9f589d9fd92605c065f90e11b6c3
Beatriz <beatriz.mediatto2@gmail.com>
2022-08-30 15:01:07 -0300

    requisito 7 concluido

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -11,4 +11,16 @@ describe('Testes da função HandlerElephants', () => {
   test('Testa se o argumento names retorna um array de nomes que possui o nome Jefferson', () => {
     expect(handlerElephants('names')).toContain('Jefferson');
   })
+  test('Testa se o argumento location retorna a string NW', () => {
+    expect(handlerElephants('location')).toMatch(/NW/);
+  })
+  test('Testa se o argumento popularity retorna um número igual ou maior a 5', () => {
+    expect(handlerElephants('popularity')).toBeGreaterThanOrEqual(5);
+  })
+  test('Testa se o argumento availability retorna um array de dias da semana que não contém Monday', () => {
+    expect(handlerElephants('availability')).not.toContain('Monday');
+  })
+  test('Testa se não passar nenhum parametro a função retorna undefined', () => {
+    expect(handlerElephants()).toBeUndefined();
+  })
 });
