de3022e0e9f3e666f43e54250405b64146176031
Beatriz <beatriz.mediatto2@gmail.com>
2022-08-31 16:53:45 -0300

    requisito 8 e 10 resolvido com ajuda do Edu

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -3,27 +3,51 @@ const data = require('../data/zoo_data');
 function countEntrants(entrants) {
   return entrants.reduce((acc, elemento) => {
     if (elemento.age < 18) {
-      countChild += 1
-      acc.child = countChild
+      acc.child += 1;
+      // console.log(acc.child);
       return acc;
     }
     if (elemento.age >= 18 && elemento.age < 50) {
-      countAdult += 1;
-      acc.adult = countAdult;
+      acc.adult += 1;
+      // console.log(acc.adult)
       return acc;
     }
-    if (elemento.age > 50) {
-      countSenior += 1
-      acc.senior = countSenior;
+    if (elemento.age >= 50) {
+      acc.senior += 1;
+      // console.log(acc.senior);
       return acc;
     }
-    acc.adult = countAdult;
-    acc.senior = countSenior;
-  }, {})
+  }, {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  })
 }
+// console.log(countEntrants([
+// 	{ name:  'Lara Carvalho', age:  5 },
+// 	{ name:  'Frederico Moreira', age:  5 },
+// 	{ name:  'Pedro Henrique Carvalho', age:  5 },
+// 	{ name:  'Maria Costa', age:  18 },
+// 	{ name:  'Núbia Souza', age:  18 },
+// 	{ name:  'Carlos Nogueira', age:  50 },
+// ]));
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  const { prices } = data;
+  const { adult, senior, child } = prices;
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  let ingresso = 0;
+  ingresso += child * countEntrants(entrants).child + adult * countEntrants(entrants).adult + senior * countEntrants(entrants).senior;
+  return ingresso;
 }
-
+console.log(calculateEntry([
+	{ name:  'Lara Carvalho', age:  5 },
+	{ name:  'Frederico Moreira', age:  5 },
+	{ name:  'Pedro Henrique Carvalho', age:  5 },
+	{ name:  'Maria Costa', age:  18 },
+	{ name:  'Núbia Souza', age:  18 },
+	{ name:  'Carlos Nogueira', age:  50 },
+]));
 module.exports = { calculateEntry, countEntrants };
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
+const { employees, species } = data;
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animal = employees.find((elemento) => elemento.id === id).responsibleFor[0];
+  // console.log(animal);
+  const animalVelho = species.find((elemento) => elemento.id === animal)
+    .residents.sort((a, b) => b.age - a.age)[0];
+  return Object.values(animalVelho);
 }
-
+// console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
