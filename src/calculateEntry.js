7423c12ba17de8e94dc56f0ef3f145e928aeb3f3
Beatriz <beatriz.mediatto2@gmail.com>
2022-08-31 14:36:58 -0300

    primeira parte requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-
+  return entrants.reduce((acc, elemento) => {
+    if (elemento.age < 18) {
+      countChild += 1
+      acc.child = countChild
+      return acc;
+    }
+    if (elemento.age >= 18 && elemento.age < 50) {
+      countAdult += 1;
+      acc.adult = countAdult;
+      return acc;
+    }
+    if (elemento.age > 50) {
+      countSenior += 1
+      acc.senior = countSenior;
+      return acc;
+    }
+    acc.adult = countAdult;
+    acc.senior = countSenior;
+  }, {})
 }
 
 function calculateEntry(entrants) {
