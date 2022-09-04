0a95b66a767b78add45f8f837d6070a48c7464ca
Beatriz <beatriz.mediatto2@gmail.com>
2022-09-04 12:47:54 -0300

    arrumando erro de lint

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,19 +1,7 @@
 const data = require('../data/zoo_data');
+
 const { species, hours } = data;
 const diasDaSemana = Object.keys(hours);
-
-function getSchedule(scheduleTarget) {
-  const dia = horariosZoo();
-  if (species.some((elemento) => scheduleTarget === elemento.name)) {
-    return species.find((el) => el.name === scheduleTarget).availability;
-  }
-  if (diasDaSemana.includes(scheduleTarget)) {
-    return { [scheduleTarget]: dia[scheduleTarget], };
-  }
-  if (scheduleTarget === undefined || scheduleTarget !== diasDaSemana) {
-    return dia;
-  }
-}
 
 const horariosZoo = () => {
   const horarios = {};
@@ -21,17 +9,30 @@ const horariosZoo = () => {
     if (elemento !== 'Monday') {
       horarios[elemento] = {
         officeHour: `Open from ${hours[elemento].open}am until ${hours[elemento].close}pm`,
-        exhibition: species.filter((element) => element.availability.includes(elemento)).map((el) => el.name)
+        exhibition: species.filter((element) => element.availability.includes(elemento))
+          .map((el) => el.name),
       };
-    }
-    else {
+    } else {
       horarios[elemento] = {
-        officeHour: `CLOSED`,
-        exhibition: `The zoo will be closed!`,
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
       };
     }
   });
   return horarios;
 };
+
+function getSchedule(scheduleTarget) {
+  const dia = horariosZoo();
+  if (species.some((elemento) => scheduleTarget === elemento.name)) {
+    return species.find((el) => el.name === scheduleTarget).availability;
+  }
+  if (diasDaSemana.includes(scheduleTarget)) {
+    return { [scheduleTarget]: dia[scheduleTarget] };
+  }
+  if (scheduleTarget === undefined || scheduleTarget !== diasDaSemana) {
+    return dia;
+  }
+}
 
 module.exports = getSchedule;
